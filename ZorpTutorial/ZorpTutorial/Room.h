399a0eee90f384288cfaebce56bfa2c0cd375a1a
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-02-21 16:54:21 +1030

    Starting part 14

--- ZorpTutorial/ZorpTutorial/Room.h
+++ ZorpTutorial/ZorpTutorial/Room.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "Point2D.h"
 
+class Powerup;
+class Player;
+
 class Room
 {
 public:
@@ -15,10 +18,14 @@ public:
 	void draw();
 	void drawDescription();
 	
-	bool executeCommand(int command);
+	bool executeCommand(int command, Player* pPlayer);
 
 private:
+	bool pickup(Player* pPlayer);
+
 	Point2D m_mapPosition;
 	int m_type;
+
+	Powerup* m_powerup;
 };
 
